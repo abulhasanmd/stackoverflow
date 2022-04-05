2e8dbe97f20071a5a3d2c69fb043d2f3aa4a5a2c
apparajuakhil <94913578+apparajuakhil@users.noreply.github.com>
2022-04-04 18:49:17 -0700

    adding vote service & consumer

--- backend/services/voteService.js
+++ backend/services/voteService.js
@@ -1,36 +1,85 @@
 const Vote = require('../models/Vote');
-const uuid = require('uuidv4');
-
-
-const getVotesForQuestionId = async (body) => {
-  console.log('Entering voteService.getVotesByResourceId');
-  try {
-    const votesResponse = await Vote.find({ resourceId: body.resourceId }).exec();
-    console.log(`get vote respoinse :${voteResponse}`);
-    return { data: votesResponse };
-  } catch (e) {
-    console.error('Exception occurred while getting votes', e);
-    return { error: { message: e.message } };
-  }
-};
+const Question = require('../models/Question');
 
 const addVote = async (body) => {
-  console.log(`Entering voteService.addVote,payload is ${body}`);
-  try {
-    const newVoteId = `c-${uuid()}`
-    const vote = {_id: newVoteId, ...body }
-    const voteResponse = await Vote.create(vote);
-    console.log(`add vote respoinse :${voteResponse}`);
-    if (voteResponse) {
-      return { data: { message: `Vote ${newVoteId} created Successfully` } };
+    console.log(`Entering voteService.addVote,payload is ${body}`);
+    try {
+        const vote = {
+            ...body
+        };
+        const voteRecord = await Vote.find({
+            $and: [{
+                resourceId: body.resourceId
+            }, {
+                createdBy: body.createdBy
+            }]
+        });
+        if (!voteRecord.length) {
+            const voteResponse = await Vote.create(vote);
+            console.log(`add vote respoinse :${voteResponse}`);
+            const questionResponse = await Question.updateOne({
+                _id: body.resourceId
+            }, {
+                $inc: {
+                    score: vote.score
+                }
+            });
+            console.log(`update question respoinse :${questionResponse}`);
+            if (voteResponse) {
+                return {
+                    data: {
+                        message: `Vote created Successfully`
+                    }
+                };
+            }
+        } else {
+            if ((voteRecord.score > 0 && vote.score < 0) || (voteRecord.score < 0 && vote.score > 0)) {
+                const voteResponse = await Vote.updateOne({
+                    _id: voteRecord[0]._id
+                }, {
+                    $set: {
+                        score: vote.score
+                    }
+                });
+                console.log(`update vote respoinse :${voteResponse}`);
+                const questionResponse = await Question.updateOne({
+                    _id: body.resourceId
+                }, {
+                    $inc: {
+                        score: vote.score * 2
+                    }
+                });
+                console.log(`update question respoinse :${questionResponse}`);
+                if (voteResponse) {
+                    return {
+                        data: {
+                            message: `Vote ${voteRecord[0]._id} updated Successfully`
+                        }
+                    };
+                }
+            } else if ((voteRecord.score > 0 && vote.score > 0) || (voteRecord.score < 0 && vote.score < 0)) {
+                return {
+                    error: {
+                        message: 'Vote already exists'
+                    }
+                };
+            }
+        }
+        return {
+            error: {
+                message: 'Some error occured while creating vote'
+            }
+        };
+    } catch (e) {
+        console.error('Exception occurred while creating vote', e);
+        return {
+            error: {
+                message: e.message
+            }
+        };
     }
-    return { error: { message: 'Some error occured while creating vote' } };
-  } catch (e) {
-    console.error('Exception occurred while creating vote', e);
-    return { error: { message: e.message } };
-  }
 };
 
 module.exports = {
-    getVotesByResourceId, addVote
+    addVote
 };
