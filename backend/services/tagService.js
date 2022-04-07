1c925f94f08cfc41788f518a053df45f70d29f1c
sharansantosh97 <sharan.santosh.ss@gmail.com>
2022-04-07 08:33:57 -0700

    Tags search added and questions with tag in last seven days filtered

--- backend/services/tagService.js
+++ backend/services/tagService.js
@@ -1,14 +1,48 @@
-const Tag = require('../models/Tag');
+const TagsModel = require('../models/Tag');
+const QuestionsModel = require('../models/Question');
 
 
-const getTags = async (msg) => {
+const getTags = async (msg) => 
+{
+	let searchWord = msg.query.search;
 	try 
     {
-		const tags = await Tag.find({ }).lean();
-        tags.sort((a, b) => (a.questionsCount > b.questionsCount) ? -1 : 1);
-        return {
-			data: tags,
-		};
+		if (searchWord) 
+        {
+            const tags = await TagsModel.find({name:{$regex:searchWord, $options: 'i'}}).limit(5).lean();
+            let lastWeek = new Date();
+            lastWeek.setDate(lastWeek.getDate() -7);
+            for(let i=0;i<tags.length;i++)
+            {
+                let tagId = tags[i]._id.toString();
+                const que = await QuestionsModel.find({$and: [{tags: tagId}, { "createdOn": { $gte: lastWeek}} ]}).lean();
+                let numberOfQuestionsByDate = que.length;
+                tags[i].noOfQuestionsInSevenDays = numberOfQuestionsByDate;
+            }       
+            
+            tags.sort((a, b) => (a.questionsCount > b.questionsCount) ? -1 : 1);
+            return {
+				data: tags,
+			};
+        
+        } else 
+        {
+            const tags = await TagsModel.find({ }).lean();
+            let lastWeek = new Date();
+            lastWeek.setDate(lastWeek.getDate() -7);
+            for(let i=0;i<tags.length;i++)
+            {
+                let tagId = tags[i]._id.toString();
+                const que = await QuestionsModel.find({$and: [{tags: tagId}, { "createdOn": { $gte: lastWeek}} ]}).lean();
+                let numberOfQuestionsByDate = que.length;
+                tags[i].noOfQuestionsInSevenDays = numberOfQuestionsByDate;
+            } 
+
+            tags.sort((a, b) => (a.questionsCount > b.questionsCount) ? -1 : 1);
+            return {
+				data: tags,
+			};
+        }
 	} 
     catch (e) 
     {
