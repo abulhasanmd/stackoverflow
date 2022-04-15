cacb1be9ff35c03fcfb992dfad3cfe652c2a6088
sharansantosh97 <sharan.santosh.ss@gmail.com>
2022-04-15 09:57:48 -0700

    SideBar Componenet Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/questions-tab/SideBar';
 
 function App() {
-	return <div className="App"></div>;
+	return <div className="App">
+		<h1>Hello</h1>
+	</div>;
 }
 
 export default App;
