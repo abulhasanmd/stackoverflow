63bde964eb795f71f469c5c252674291e5d2d36b
abulhasan <abulhasan.swe@gmail.com>
2022-04-24 22:54:18 -0700

    Added backend structure

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const logger = require('morgan');
+require('dotenv').config();
+const cors = require('cors');
+
+const corsOptions = {
+  origin: true,
+
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+
+  credentials: true,
+
+  optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+const usersRouter = require('./routes/userRouter');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/users', usersRouter);
+
+app.use((err, req, res, next) => {
+  console.error('in error handler');
+  // set locals, only providing error in development
+
+  res.locals.message = err.message;
+
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 400).json({ error: err.message });
+});
+
+module.exports = app;
