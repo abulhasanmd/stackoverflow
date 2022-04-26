4206f66da15d99bc9267dcbda21e43ccb4f72cf9
Krishna sai Patel <herorocks56@gmail.com>
2022-04-26 15:27:15 -0700

    Modified Register Page and added styles

--- frontend/src/pages/Register/RegisterPage.jsx
+++ frontend/src/pages/Register/RegisterPage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {Redirect} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const RegisterPage = () => {
+// import {setAlert} from '../../redux/alert/alert.actions';
+
+import Caption from './Caption/Caption';
+import AuthForm from '../../components/auth-form/AuthForm';
+
+import './Register.styles.scss';
+
+const Register = ({isAuthenticated}) => {
+  if (isAuthenticated) {
+    return <Redirect to='/' />;
+  }
+
   return (
-    <div>RegisterPage</div>
-  )
-}
+      <div className='auth-page'>
+        <div className='register-content'>
+          <div className='register-grid'>
+            <Caption />
+            <AuthForm action={'Sign up'} />
+          </div>
+        </div>
+      </div>
+  );
+};
 
-export default RegisterPage
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+
+export default Register;
