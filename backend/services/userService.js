4c50e3d9a12b9403b59c71e6d855c49606246b7a
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2022-04-30 10:56:55 -0700

    Fixing minor query issue and disabling indentation check

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -6,71 +6,71 @@ const Event = require('../models/Event');
 const questionConstants = require('../constants/questionConstants');
 
 const getTagsUsedInQuestions = async (userId) => {
-  try {
-    const allTags = await Question.find({
-      createdBy: userId,
-    }).populate(tags).select({
-      tags,
-    });
-    const mergedTags = {};
-    allTags.forEach((tags) => {
-      tags.forEach((tag) => {
-        mergedTags[tag.name] = tag;
-      });
-    });
-    return {
-      data: Object.values(allTags),
-    };
-  } catch (e) {
-    console.error('Exception occurred while getting tags', e);
-    return {
-      error: {
-        message: e.message,
-      },
-    };
-  }
+	try {
+		const allTags = await Question.find({
+			createdBy: userId,
+		}).populate('tags').select({
+			tags: 1,
+		});
+		const mergedTags = {};
+		allTags.forEach((tags) => {
+			tags.forEach((tag) => {
+				mergedTags[tag.name] = tag;
+			});
+		});
+		return {
+			data: Object.values(allTags),
+		};
+	} catch (e) {
+		console.error('Exception occurred while getting tags', e);
+		return {
+			error: {
+				message: e.message,
+			},
+		};
+	}
 };
 
 const getBookmarks = async (userId) => {
-  try {
-    const bookmarks = await User.findById({
-      userId,
-    }).select({
-      bookmarks,
-    }).exec();
-    return {
-      data: bookmarks,
-    };
-  } catch (e) {
-    console.error('Exception occured while fetching bookmarks', e);
-    return {
-      error: {
-        message: e.message,
-      },
-    };
-  }
+	try {
+		const bookmarks = await User.findById({
+			userId,
+		}).select({
+			bookmarks,
+		}).exec();
+		return {
+			data: bookmarks,
+		};
+	} catch (e) {
+		console.error('Exception occured while fetching bookmarks', e);
+		return {
+			error: {
+				message: e.message,
+			},
+		};
+	}
 };
 
 const getReputationActivity = async (userId) => {
-  try {
-    const events = await Event.find({
-      affectedUser: userId,
-    }).exec();
-    return {
-      data: events,
-    };
-  } catch (e) {
-    console.error('Error while fetching reputation activity', e);
-    return {
-      error: {
-        message: e.message,
-      },
-    };
-  }
+	try {
+		const events = await Event.find({
+			affectedUser: userId,
+		}).exec();
+		return {
+			data: events,
+		};
+	} catch (e) {
+		console.error('Error while fetching reputation activity', e);
+		return {
+			error: {
+				message: e.message,
+			},
+		};
+	}
 };
 
 module.exports = {
-  getTagsUsedInQuestions,
-  getBookmarks,
-  getReputationActivity,
+	getTagsUsedInQuestions,
+	getBookmarks,
+	getReputationActivity,
 };
