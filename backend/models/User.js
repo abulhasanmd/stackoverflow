1e624ebe65292472b0240e2e0fa595bff27f4048
Abul Hasan <abulhasan.swe@gmail.com>
2022-04-30 14:05:51 -0700

    merged conflict

---
+++ backend/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  _id: { type: String },
+  name: { type: String, maxLength: 20 },
+  emailId: { type: String, maxLength: 20 },
+  role: { type: String, enum: ['admin', 'user'] },
+  password: { type: String, minLength: 8 },
+  imageUrl: { type: String, maxLength: 100 },
+  location: { type: String, maxLength: 20 },
+  createdOn: { type: Date, default: Date.now },
+  modifiedOn: { type: Date, default: Date.now },
+  reputation: { type: Number, default: 0 },
+  questionsAnswered: { type: Number, default: 0 },
+  questionsAsked: { type: Number, default: 0 },
+  lastSeen: { type: Date, default: Date.now },
+  reach: { type: Number, default: 0 },
+  about: { type: String, maxLength: 20 },
+  badges: {
+    _id: {
+      type: { type: String, default: 0 },
+      name: { type: String, maxLength: 20 },
+      createdBy: { type: String, maxLength: 20 },
+      score: { type: Number, default: 0 },
+      level: { type: String, maxLength: 10 },
+    },
+  },
+  bookmarks: { type: Array },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
