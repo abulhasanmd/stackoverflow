0a480a40c847e293ebc5a4b633249f196ab88577
Abul Hasan <abulhasan.swe@gmail.com>
2022-05-04 08:55:04 -0700

    fetching broker host from .env file

--- backend/kafka/kafkaClient.js
+++ backend/kafka/kafkaClient.js
@@ -3,7 +3,7 @@ const { Kafka } = require('kafkajs');
 // Initialize Kafka Client
 const kafka = new Kafka({
   clientId: 'middleware-system',
-  brokers: ['localhost:9092'],
+  brokers: [process.env.BROKER_HOST],
 });
 
 module.exports = { kafka };
