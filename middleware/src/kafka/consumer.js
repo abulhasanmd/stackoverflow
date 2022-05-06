bdece576684addb7c403bda562aab0fc2bb5636d
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2022-05-06 00:24:16 -0700

    Adding heartbeat and session timeout configs

--- middleware/src/kafka/consumer.js
+++ middleware/src/kafka/consumer.js
@@ -1,63 +1,78 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-unused-vars */
-const { kafka } = require('./kafkaClient');
+const {
+	kafka
+} = require('./kafkaClient');
 
-const consumer = kafka.consumer({ groupId: 'middleware-consumer' });
+const consumer = kafka.consumer({
+	groupId: 'middleware-consumer',
+	sessionTimeout: 10000,
+	heartbeatInterval: 2000,
+});
 const idToCallBackMap = {};
 let partition;
 (async () => {
-  await consumer.connect();
-  await consumer.subscribe({
-    topic: process.env.RESPONSE_TOPIC,
-    fromBeginning: true,
-  });
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      responseHandler(message);
-    },
-  });
+	await consumer.connect();
+	await consumer.subscribe({
+		topic: process.env.RESPONSE_TOPIC,
+		fromBeginning: true,
+	});
+	await consumer.run({
+		eachMessage: async ({
+			topic,
+			partition,
+			message
+		}) => {
+			responseHandler(message);
+		},
+	});
 })();
 
 consumer.on(consumer.events.GROUP_JOIN, (event) => {
-  partition = `${event.payload.memberAssignment.responses[0]}`;
-  console.log(`listening on partition: ${partition}`);
+	partition = `${event.payload.memberAssignment.responses[0]}`;
+	console.log(`listening on partition: ${partition}`);
 });
 
 const addCallBacktoCallBackMap = async (id, callback) => {
-  const tId = setTimeout(
-    () => {
-      callback('Request Timeout, Please try again!', null);
-      delete idToCallBackMap.id;
-    },
-    600000,
-    id,
-  );
-  idToCallBackMap[id] = { callback, tId };
-  return partition;
+	const tId = setTimeout(
+		() => {
+			callback('Request Timeout, Please try again!', null);
+			delete idToCallBackMap.id;
+		},
+		600000,
+		id,
+	);
+	idToCallBackMap[id] = {
+		callback,
+		tId
+	};
+	return partition;
 };
 
 const responseHandler = async (message) => {
-  const id = message.headers.id.toString();
-  try {
-    const messageJSON = JSON.parse(message.value.toString());
-    // console.log('id from header is ', id);
-    // console.log('maps is',idToCallBackMap);
-    // console.log('messagejson is', JSON.stringify(messageJSON));
-    const entry = idToCallBackMap[id];
-    if (entry) {
-      if (messageJSON.data) {
-        entry.callback(null, messageJSON.data);
-      } else {
-        entry.callback(messageJSON.error, null);
-      }
-      delete idToCallBackMap.id;
-      clearTimeout(entry.tId);
-    } else {
-      console.error('response received after slo', messageJSON);
-    }
-  } catch (error) {
-    console.error(error);
-  }
+	const id = message.headers.id.toString();
+	try {
+		const messageJSON = JSON.parse(message.value.toString());
+		// console.log('id from header is ', id);
+		// console.log('maps is',idToCallBackMap);
+		// console.log('messagejson is', JSON.stringify(messageJSON));
+		const entry = idToCallBackMap[id];
+		if (entry) {
+			if (messageJSON.data) {
+				entry.callback(null, messageJSON.data);
+			} else {
+				entry.callback(messageJSON.error, null);
+			}
+			delete idToCallBackMap.id;
+			clearTimeout(entry.tId);
+		} else {
+			console.error('response received after slo', messageJSON);
+		}
+	} catch (error) {
+		console.error(error);
+	}
 };
 
-module.exports = { addCallBacktoCallBackMap };
+module.exports = {
+	addCallBacktoCallBackMap
+};
