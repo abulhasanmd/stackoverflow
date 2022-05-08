28c67f27c9b02decc63f99e6ba9a1601b845ac9f
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2022-05-07 17:39:01 -0700

    Editing index.js

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import PostQuestion from './components/post-question/PostQuestion';
+import App from './App';
+//import PostQuestion from './components/post-question/PostQuestion';
 //import UserProfile from './components/user-profile/UserProfile';
 //import UserActivityTab from './components/user-activity-tab/UserActivityTab';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<PostQuestion />
+		<App />
 	</React.StrictMode>,
 );
 
