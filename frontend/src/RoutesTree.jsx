c932895bc3bf5ee46f8ec115fb2c7bbc1edd4b07
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2022-05-09 18:51:09 -0700

    Adding react-icons

--- frontend/src/RoutesTree.jsx
+++ frontend/src/RoutesTree.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/Login/LoginPage';
 import NotFoundPage from './pages/NotFound/NotFoundPage';
-import withPageTitle from "./components/withPageTitle/withPageTitle"
+import withPageTitle from './components/withPageTitle/withPageTitle';
 import LayoutWrapper from './components/LayoutWrapper/LayoutWrapper.component';
 import RegisterPage from './pages/Register/RegisterPage';
 import AllTagsPage from './pages/AllTagsPage/AllTagsPage';
 import QuestionsPage from './pages/QuestionsPage/QuestionsPage';
-import AskQuestionPage from './pages/AskQuestionPage/AskQuestionPage';
+// import AskQuestionPage from './pages/AskQuestionPage/AskQuestionPage';
 import AdminAnalytics from './components/admin-analytics/AdminAnalytics';
 //import AskQuestionPage from './pages/AskQuestionPage/AskQuestionPage';
 
@@ -19,21 +19,28 @@ import AdminAnalytics from './components/admin-analytics/AdminAnalytics';
 //import TagPage from './pages/TagPage/TagPage';
 
 const AllTagsPageComponent = withPageTitle({
-	component: LayoutWrapper({component: AllTagsPage}),
+	component: LayoutWrapper({ component: AllTagsPage }),
 	title: 'Tags - CLONE Stack Overflow',
-  });
+});
 
-  const QuestionsPageComponent = withPageTitle({
-	component: LayoutWrapper({component: QuestionsPage}),
+const QuestionsPageComponent = withPageTitle({
+	component: LayoutWrapper({ component: QuestionsPage }),
 	title: 'All Questions - CLONE Stack Overflow',
-  });
-  
+});
 
 const RoutesTree = () => {
 	return (
 		<Routes>
-			<Route exact path="/questions" element={<QuestionsPageComponent />} />
-			<Route exact path="/add/question" element={<AllTagsPageComponent />} />
+			<Route
+				exact
+				path="/questions"
+				element={<QuestionsPageComponent />}
+			/>
+			<Route
+				exact
+				path="/add/question"
+				element={<AllTagsPageComponent />}
+			/>
 			{/* <Route exact path='/tags/:tagname' component={TagPageComponent} /> */}
 			<Route exact path="/tags" element={<AllTagsPage />} />
 			<Route exact path="/register" element={<RegisterPage />} />
