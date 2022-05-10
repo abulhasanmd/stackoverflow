19c70e79ae9b3337b3ad9e90fc0f5c73f4be2c47
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2022-05-10 16:20:08 -0700

    Editing User Model to add metrics

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,30 +1,119 @@
 const mongoose = require('mongoose');
 
+const tagsInformationSchema = new mongoose.Schema({
+	name: {
+		type: String,
+	},
+	descr: {
+		type: String,
+	},
+	score: {
+		type: Number,
+		default: 0,
+	},
+	posts: {
+		type: Number,
+		default: 0,
+	},
+});
 const userSchema = new mongoose.Schema({
-  name: { type: String, maxLength: 20 },
-  emailId: { type: String, maxLength: 20 },
-  role: { type: String, enum: ['admin', 'user'] },
-  password: { type: String, minLength: 8 },
-  imageUrl: { type: String, maxLength: 100 },
-  location: { type: String, maxLength: 20 },
-  createdOn: { type: Date, default: Date.now },
-  modifiedOn: { type: Date, default: Date.now },
-  reputation: { type: Number, default: 0 },
-  // questionsAnswered: { type: Number, default: 0 },
-  // questionsAsked: { type: Number, default: 0 },
-  lastSeen: { type: Date, default: Date.now },
-  reach: { type: Number, default: 0 },
-  about: { type: String, maxLength: 20 },
-  badges: {
-    _id: {
-      type: { type: String, default: 0 },
-      name: { type: String, maxLength: 20 },
-      createdBy: { type: String, maxLength: 20 },
-      score: { type: Number, default: 0 },
-      level: { type: String, maxLength: 10 },
-    },
-  },
-  bookmarks: { type: Array, default : [] },
+	name: {
+		type: String,
+		maxLength: 20,
+	},
+	emailId: {
+		type: String,
+		maxLength: 20,
+	},
+	role: {
+		type: String,
+		enum: ['admin', 'user'],
+	},
+	password: {
+		type: String,
+		minLength: 8,
+	},
+	imageUrl: {
+		type: String,
+		maxLength: 100,
+	},
+	location: {
+		type: String,
+		maxLength: 20,
+	},
+	createdOn: {
+		type: Date,
+		default: Date.now,
+	},
+	modifiedOn: {
+		type: Date,
+		default: Date.now,
+	},
+	reputation: {
+		type: Number,
+		default: 0,
+	},
+	tagsInformation: [tagsInformationSchema],
+	questionsAsked: {
+		type: Number,
+		default: 0,
+	},
+	answersGiven: {
+		type: Number,
+		default: 0,
+	},
+	upvotesGiven: {
+		type: Number,
+		default: 0,
+	},
+	downvotesGiven: {
+		type: Number,
+		default: 0,
+	},
+	commentsGiven: {
+		type: Number,
+		default: 0,
+	},
+	lastSeen: {
+		type: Date,
+		default: Date.now,
+	},
+	reach: {
+		type: Number,
+		default: 0,
+	},
+	about: {
+		type: String,
+		maxLength: 20,
+	},
+	badges: {
+		_id: {
+			type: {
+				type: String,
+				default: 0,
+			},
+			name: {
+				type: String,
+				maxLength: 20,
+			},
+			createdBy: {
+				type: String,
+				maxLength: 20,
+			},
+			score: {
+				type: Number,
+				default: 0,
+			},
+			level: {
+				type: String,
+				maxLength: 10,
+			},
+		},
+	},
+	bookmarks: {
+		type: Array,
+		default: [],
+	},
 });
 
 const User = mongoose.model('User', userSchema);
