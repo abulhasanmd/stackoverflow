9564e10a5d5ebacbb2bdf018943ecc84e3df1e6c
Krishna sai Patel <herorocks56@gmail.com>
2022-05-11 14:35:14 -0700

    posts redux done

--- frontend/src/redux/posts/posts.actions.js
+++ frontend/src/redux/posts/posts.actions.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios"
 
-import config from "../../config";
-import {setAlert} from '../alert/alert.actions';
+import config from "../../config"
+import { setAlert } from "../alert/alert.actions"
 import {
   GET_POSTS,
   GET_POST,
@@ -10,130 +10,138 @@ import {
   POST_ERROR,
   DELETE_POST,
   ADD_POST,
-} from './posts.types';
+} from "./posts.types"
 
 // Get posts
-export const getPosts = () => async (dispatch) => {
-  try {
-    const res = await axios.get(config.BASE_URL + '/api/posts');
+export const getPosts =
+  (searchQuery = "") =>
+  async (dispatch) => {
+    try {
+      const res = await axios.post(config.BASE_URL + "/posts/get-posts", {
+        searchq: searchQuery,
+      })
 
-    dispatch({
-      type: GET_POSTS,
-      payload: res.data.data,
-    });
-  } catch (err) {
-    dispatch(setAlert(err.response.data.message, 'danger'));
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data.data,
+      })
+    } catch (err) {
+      dispatch(setAlert(err.response.data.message, "danger"))
 
-    dispatch({
-      type: POST_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status},
-    });
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      })
+    }
   }
-};
 
 // Get post
 export const getPost = (id) => async (dispatch) => {
   try {
-    const res = await axios.get(config.BASE_URL + `/api/posts/${id}`);
+    const res = await axios.get(config.BASE_URL + `/api/posts/${id}`)
 
     dispatch({
       type: GET_POST,
       payload: res.data.data,
-    });
+    })
   } catch (err) {
-    dispatch(setAlert(err.response.data.message, 'danger'));
+    dispatch(setAlert(err.response.data.message, "danger"))
 
     dispatch({
       type: POST_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status},
-    });
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
   }
-};
+}
 
 //GET TOP POSTS
 export const getTopPosts = () => async (dispatch) => {
   try {
-    const res = await axios.get(config.BASE_URL + '/api/posts/top');
+    const res = await axios.get(config.BASE_URL + "/api/posts/top")
 
     dispatch({
       type: GET_TOP_POSTS,
       payload: res.data.data,
-    });
+    })
   } catch (err) {
-    dispatch(setAlert(err.response.data.message, 'danger'));
+    dispatch(setAlert(err.response.data.message, "danger"))
 
     dispatch({
       type: POST_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status},
-    });
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
   }
-};
+}
 
 //GET TAG POSTS
 export const getTagPosts = (tagName) => async (dispatch) => {
   try {
-    const res = await axios.get(config.BASE_URL + `/api/posts/tag/${tagName}`);
+    const res = await axios.get(config.BASE_URL + `/api/posts/tag/${tagName}`)
 
     dispatch({
       type: GET_TAG_POSTS,
       payload: res.data.data,
-    });
+    })
   } catch (err) {
-    dispatch(setAlert(err.response.data.message, 'danger'));
+    dispatch(setAlert(err.response.data.message, "danger"))
 
     dispatch({
       type: POST_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status},
-    });
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
   }
-};
+}
 
 // Add post
 export const addPost = (formData) => async (dispatch) => {
   const config_headers = {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  };
+  }
 
   try {
-    const res = await axios.post(config.BASE_URL + '/api/posts', formData, config_headers);
+    const res = await axios.post(
+      config.BASE_URL + "/api/posts",
+      formData,
+      config_headers
+    )
 
     dispatch({
       type: ADD_POST,
       payload: res.data.data,
-    });
+    })
 
-    dispatch(setAlert(res.data.message, 'success'));
+    dispatch(setAlert(res.data.message, "success"))
 
-    dispatch(getPosts());
+    dispatch(getPosts())
   } catch (err) {
-    dispatch(setAlert(err.response.data.message, 'danger'));
+    dispatch(setAlert(err.response.data.message, "danger"))
 
     dispatch({
       type: POST_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status},
-    });
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
   }
-};
+}
 
 // Delete post
 export const deletePost = (id) => async (dispatch) => {
   try {
-    const res = await axios.delete(config.BASE_URL + `/api/posts/${id}`);
+    const res = await axios.delete(config.BASE_URL + `/api/posts/${id}`)
 
     dispatch({
       type: DELETE_POST,
       payload: id,
-    });
+    })
 
-    dispatch(setAlert(res.data.message, 'success'));
+    dispatch(setAlert(res.data.message, "success"))
   } catch (err) {
-    dispatch(setAlert(err.response.data.message, 'danger'));
+    dispatch(setAlert(err.response.data.message, "danger"))
 
     dispatch({
       type: POST_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status},
-    });
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
   }
-};
+}
--- frontend/src/redux/posts/posts.reducer.js
+++ frontend/src/redux/posts/posts.reducer.js
@@ -6,14 +6,14 @@ import {
   POST_ERROR,
   DELETE_POST,
   ADD_POST,
-} from './posts.types';
+} from "./posts.types"
 
 const initialState = {
   posts: [],
   post: null,
   loading: true,
   error: {},
-};
+}
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
@@ -22,34 +22,34 @@ export default function posts(state = initialState, action) {
     case GET_TAG_POSTS:
       return {
         ...state,
-        posts: action.payload,
+        posts: action.payload.posts,
         loading: false,
-      };
+      }
     case GET_POST:
       return {
         ...state,
         post: action.payload,
         loading: false,
-      };
+      }
     case ADD_POST:
       return {
         ...state,
         posts: [action.payload, ...state.posts],
         loading: false,
-      };
+      }
     case DELETE_POST:
       return {
         ...state,
         posts: state.posts.filter((post) => post.id !== action.payload),
         loading: false,
-      };
+      }
     case POST_ERROR:
       return {
         ...state,
         error: action.payload,
         loading: false,
-      };
+      }
     default:
-      return state;
+      return state
   }
 }
