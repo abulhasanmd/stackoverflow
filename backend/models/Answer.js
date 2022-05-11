2208d0c115026d51bd9623c0365f553a4c2c0119
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2022-05-10 23:45:39 -0700

    Updating Events model

--- backend/models/Answer.js
+++ backend/models/Answer.js
@@ -2,21 +2,43 @@ const mongoose = require('mongoose');
 const mongooseConnection = require('./mongooseConnection');
 
 const answerSchema = new mongoose.Schema({
-    questionId: { 
-        type: mongoose.Schema.Types.ObjectId, ref: 'Question', 
-        required: true },
-    createdBy: {
-        _id: { 
-            type: mongoose.Schema.Types.ObjectId, ref: 'User', 
-            required: true 
-        },
-        imageUrl: { type: String, maxLength: 100, required: true },
-    },
-    createdOn: { type: Date, default: Date.now },
-    answer: { type: String, required: true },
-    score: { type: Number, default: 0 },
-    vote: { type: Number, default: 0 },
-    isBestAnswer: { type: Boolean, default: false }, 
+	questionId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Question',
+		required: true,
+	},
+	createdBy: {
+		_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		imageUrl: {
+			type: String,
+			maxLength: 100,
+			required: true,
+		},
+	},
+	createdOn: {
+		type: Date,
+		default: Date.now,
+	},
+	answer: {
+		type: String,
+		required: true,
+	},
+	score: {
+		type: Number,
+		default: 0,
+	},
+	vote: {
+		type: Number,
+		default: 0,
+	},
+	isBestAnswer: {
+		type: Boolean,
+		default: false,
+	},
 });
 
 const Answer = mongooseConnection.model('Answer', answerSchema);
--- backend/models/Event.js
+++ backend/models/Event.js
@@ -2,32 +2,35 @@ const mongoose = require('mongoose');
 const mongooseConnection = require('./mongooseConnection');
 
 const eventSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    required: true,
-    maxlength: 20,
-  },
-  outcome: {
-    type: String,
-    maxLength: 20,
-  },
-  createdBy: {
-    type: String,
-    required: true,
-  },
-  createdOn: {
-    type: Date,
-    default: Date.now(),
-  },
-  affectedUser: {
-    type: Number,
-  },
-  articleType: {
-    type: String,
-  },
-  articleId: {
-    type: String,
-  },
+	type: {
+		type: String,
+		required: true,
+		maxlength: 20,
+	},
+	outcome: {
+		type: String,
+		maxLength: 20,
+	},
+	createdBy: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	createdOn: {
+		type: Date,
+		default: Date.now(),
+	},
+	affectedUser: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	articleType: {
+		type: String,
+	},
+	articleId: {
+		type: String,
+	},
 });
 
 const Event = mongooseConnection.model('Event', eventSchema);
