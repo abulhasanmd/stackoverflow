bc2039901de2bcb0dbbacda579097f4427412b17
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2022-05-11 23:31:12 -0700

    Fixing Redis error

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-mixed-operators */
+/* eslint-disable no-param-reassign */
+const _ = require('lodash');
 const Tag = require('../models/Tag');
 const Question = require('../models/Question');
 const Answer = require('../models/Answer');
 const User = require('../models/User');
 const Event = require('../models/Event');
-var _ = require('lodash')
 
 const questionConstants = require('../constants/questionConstants');
 
@@ -11,10 +13,19 @@ const getAllUsers = async (query) => {
 	try {
 		let users;
 		if (query.search) {
-            users = await User.find({name: { "$regex": query.search, "$options": "i" } }).limit(5).sort( { reputation: -1 }).exec();
-        } else {
-			users = await User.find().sort( { reputation: -1 }).exec();
-        }
+			users = await User.find({
+				name: {
+					$regex: query.search,
+					$options: 'i',
+				},
+			}).limit(5).sort({
+				reputation: -1,
+			}).exec();
+		} else {
+			users = await User.find().sort({
+				reputation: -1,
+			}).exec();
+		}
 		return {
 			data: users,
 		};
@@ -87,27 +98,48 @@ const getReputationActivity = async (userId) => {
 };
 
 async function tagsUsedByUser(userId) {
-	let questions = await Question.find({createdBy: userId}, {tags: 1, votes: 1}).exec();
-	let tagIds = _.uniq(_.concat(..._.map(questions, 'tags')))
-	let tags = Tag.find({_id: {$in: tagIds}}).exec()
-	let tagsScore = {}
-	_.forEach(tagIds, (tagId) => { tagsScore[tagId] =_.sumBy(questions, (question) => _.includes(question.tags, tagId) && question.votes || 0) })
+	const questions = await Question.find({
+		createdBy: userId,
+	}, {
+		tags: 1,
+		votes: 1,
+	}).exec();
+	const tagIds = _.uniq(_.concat(..._.map(questions, 'tags')));
+	let tags = Tag.find({
+		_id: {
+			$in: tagIds,
+		},
+	}).exec();
+	const tagsScore = {};
+	_.forEach(tagIds, (tagId) => {
+		tagsScore[tagId] = _.sumBy(questions, (question) => _.includes(question.tags, tagId) && question.votes || 0);
+	});
 	tags = _.map(tags, (tag) => {
-		tag.score = tagsScore[tag._id]
-		return tag
-	})
+		tag.score = tagsScore[tag._id];
+		return tag;
+	});
 	_.sortBy(tags, (tag) => -tag.score); // sorted in descending order
 	return tags;
 }
 
 const getUserProfile = async (userId) => {
 	try {
-		let userDetails = await User.findOne({_id: userId}).exec();
-		let questionsAsked = await Question.count({createdBy: userId}).exec();
-		let questionsAnswered = await Answer.count({'createdBy._id': userId}).exec();
-		let tagsUsed = await tagsUsedByUser(userId)
-		Object.assign(userDetails, {questionsAnswered, questionsAsked, tags: tagsUsed})
-		return userDetails
+		const userDetails = await User.findOne({
+			_id: userId,
+		}).exec();
+		const questionsAsked = await Question.count({
+			createdBy: userId,
+		}).exec();
+		const questionsAnswered = await Answer.count({
+			'createdBy._id': userId,
+		}).exec();
+		const tagsUsed = await tagsUsedByUser(userId);
+		Object.assign(userDetails, {
+			questionsAnswered,
+			questionsAsked,
+			tags: tagsUsed,
+		});
+		return userDetails;
 	} catch (e) {
 		console.error('Error while fetching getUserProfile', e);
 		return {
@@ -121,33 +153,64 @@ const getUserProfile = async (userId) => {
 async function getPosts(userId, body) {
 	let getPostsP;
 	body.filter = body.filter || 'ALL';
-	body.sortBy = body.sortBy || 'SCORE'
-	switch(body.filter) {
-		case 'ALL':
-			getPostsP = [Question.find({createdBy: userId}).limit(10), Answer.find({createdBy: userId}).limit(10)]
-		case 'QUESTIONS':
-			getPostsP =  [Question.find({createdBy: userId}).limit(10)]; break;
-		case 'ANSWERS':
-			getPostsP =  [Answer.find({createdBy: userId}).limit(10)]; break;
+	body.sortBy = body.sortBy || 'SCORE';
+	switch (body.filter) {
+	case 'ALL':
+		getPostsP = [Question.find({
+			createdBy: userId,
+		}).limit(10), Answer.find({
+			createdBy: userId,
+		}).limit(10)];
+		break;
+	case 'QUESTIONS':
+		getPostsP = [Question.find({
+			createdBy: userId,
+		}).limit(10)];
+		break;
+	case 'ANSWERS':
+		getPostsP = [Answer.find({
+			createdBy: userId,
+		}).limit(10)];
+		break;
+	default:
+		break;
 	}
 	switch (body.sortBy) {
-		case 'SCORE':
-			getPostsP = _.map(getPostsP, (getpost) => getpost.sort({score: 1})); break;
-		case 'NEWEST':
-			getPostsP = _.map(getPostsP, (getpost) => getpost.sort({createdOn: 1})); break;
+	case 'SCORE':
+		getPostsP = _.map(getPostsP, (getpost) => getpost.sort({
+			score: 1,
+		}));
+		break;
+	case 'NEWEST':
+		getPostsP = _.map(getPostsP, (getpost) => getpost.sort({
+			createdOn: 1,
+		}));
+		break;
+	default:
+		break;
 	}
 	let posts = [];
-	_.forEach(getPostsP, async (postsP) => posts.push(...await postsP))
-	posts = _.slice(_.sortBy(posts, (post) => post.score), 0, 10)
+	_.forEach(getPostsP, async (postsP) => posts.push(...await postsP));
+	posts = _.slice(_.sortBy(posts, (post) => post.score), 0, 10);
 	return posts;
 }
 
 const getUserPosts = async (data) => {
-	let {body, params, query} = data;
-	let userId = params.userId;
+	const {
+		body,
+		params,
+		query,
+	} = data;
+	const {
+		userId,
+	} = params;
 	try {
-		let response = await getPosts(userId, body)
-		return {data: {posts: response}}
+		const response = await getPosts(userId, body);
+		return {
+			data: {
+				posts: response,
+			},
+		};
 	} catch (e) {
 		console.error('Error while fetching getUserPosts', e);
 		return {
@@ -159,25 +222,47 @@ const getUserPosts = async (data) => {
 };
 
 const updateUserProfile = async (data) => {
-	let {body, params, query} = data;
-	try 
-	{
-		let imageUrl = body.imageUrl;
-		let about = body.about;
-		let location = body.location;
-		let userId = body.userId;
-		let userDetails = await User.updateOne({ _id: userId },  { $set: {"imageUrl":imageUrl, "about": about, "location": location} } ).exec();
-		
-		if(userDetails) 
-		{
-			return { data: { message: `UserProfile updated Successfully` } };
-		}else
-		{
-		  return { error: { message: 'Some error occured while updating User Profile' } };
-		}
+	const {
+		body,
+		params,
+		query,
+	} = data;
+	try {
+		const {
+			imageUrl,
+		} = body;
+		const {
+			about,
+		} = body;
+		const {
+			location,
+		} = body;
+		const {
+			userId,
+		} = body;
+		const userDetails = await User.updateOne({
+			_id: userId,
+		}, {
+			$set: {
+				imageUrl,
+				about,
+				location,
+			},
+		}).exec();
 
-	} 
-	catch (e) {
+		if (userDetails) {
+			return {
+				data: {
+					message: 'UserProfile updated Successfully',
+				},
+			};
+		}
+		return {
+			error: {
+				message: 'Some error occured while updating User Profile',
+			},
+		};
+	} catch (e) {
 		console.error('Error while fetching getUserPosts', e);
 		return {
 			error: {
@@ -194,5 +279,5 @@ module.exports = {
 	getAllUsers,
 	getUserProfile,
 	getUserPosts,
-	updateUserProfile
+	updateUserProfile,
 };
