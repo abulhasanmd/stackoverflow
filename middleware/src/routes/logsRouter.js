aef18d0c3849e272f8244d16966f9f3b38e2a4f5
saicharan <saicharancherry593@gmail.com>
2022-05-12 12:06:11 -0700

    Fixed get logs

--- middleware/src/routes/logsRouter.js
+++ middleware/src/routes/logsRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 let logsService = require('../../../backend/services/logsService.js');
 
 router.get('/:resourceId', async (req, res, next) => {
-    let {resourceId} = req.body;
+    let {resourceId} = req.params;
     let results  = await logsService.fetchLogs(resourceId)
     return res.json({data: results.response}).status(results.statusCode)
 });
