7e9880297e795e565b26e35dc5533758e8a999ff
sharansantosh97 <sharan.santosh.ss@gmail.com>
2022-05-11 23:55:45 -0700

    User Profile Page Updated

--- frontend/src/pages/ProfilePage/UserActivity/UserActivity.component.jsx
+++ frontend/src/pages/ProfilePage/UserActivity/UserActivity.component.jsx
@@ -3,72 +3,53 @@ import React from "react";
 import TagBadge from "../../../components/TagBadge/TagBadge.component";
 
 import './UserActivity.styles.css';
+//userTgs
+const UserActivity = ({userTgs}) => {
+console.log(userTgs);
 
-const UserActivity = () => (
-  <div className='grid-cell2'>
+const tagDiv = userTgs?.map((item, index) => {
+      console.log(item);
+        return (
+              <>
+                <div className='top-tags-cells'>
+                <div className='top-cell'>
+                  <div className='tag-cell bg-black-025'>
+                    <TagBadge
+                    key={index}
+                      tag_name={item.name}
+                      size={'s-tag s-tag__lg'}
+                      float={'left'}
+                    />
+                    <div className='score'>
+                      <div className='score-txt'>
+                        <div className='score-tab'>
+                          <span className='txt fc-light'>Question Count</span>
+                          <span className='number fc-black-800'>{item.questionsCount}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </>
+        );
+        });
+
+// const tagDiv = 
+
+
+
+
+  return (
+    <div className='grid-cell2'>
     <div className='top-tags'>
       <h3 className='fw-bold fc-dark bc-black-3'>Top Tags</h3>
       <div className='top-tags-sec'>
-        <div className='top-tags-cells'>
-          <div className='top-cell'>
-            <div className='tag-cell bg-black-025'>
-              <TagBadge
-                tag_name={'java'}
-                size={'s-tag s-tag__lg'}
-                float={'left'}
-              />
-              <div className='score'>
-                <div className='score-txt'>
-                  <div className='score-tab'>
-                    <span className='txt fc-light'>Posts</span>
-                    <span className='number fc-black-800'>2</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='top-tags-cells'>
-          <div className='top-cell'>
-            <div className='tag-cell bg-black-025'>
-              <TagBadge
-                tag_name={'node.js'}
-                size={'s-tag s-tag__md'}
-                float={'left'}
-              />
-              <div className='score'>
-                <div className='score-txt'>
-                  <div className='score-tab'>
-                    <span className='txt fc-light'>Posts</span>
-                    <span className='number fc-black-800'>1</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='top-tags-cells'>
-          <div className='top-cell'>
-            <div className='tag-cell bg-black-025'>
-              <TagBadge
-                tag_name={'react'}
-                size={'s-tag s-tag__md'}
-                float={'left'}
-              />
-              <div className='score'>
-                <div className='score-txt'>
-                  <div className='score-tab'>
-                    <span className='txt fc-light'>Posts</span>
-                    <span className='number fc-black-800'>0</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        {tagDiv}
       </div>
     </div>
   </div>
-)
+  )
+}
 
 export default UserActivity;
