636dbb38ed6e314e4ace5a32294c3e4d7ef436d0
Krishna sai Patel <herorocks56@gmail.com>
2022-05-12 02:59:23 -0700

    redux for register and login done

--- frontend/src/redux/auth/auth.reducer.js
+++ frontend/src/redux/auth/auth.reducer.js
@@ -6,14 +6,14 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
-} from './auth.types';
+} from "./auth.types"
 
 const initialState = {
-  token: localStorage.getItem('token'),
+  token: localStorage.getItem("token"),
   isAuthenticated: null,
   loading: true,
   user: null,
-};
+}
 
 export default function auth(state = initialState, action) {
   switch (action.type) {
@@ -23,29 +23,39 @@ export default function auth(state = initialState, action) {
         user: action.payload,
         isAuthenticated: true,
         loading: false,
-      };
+      }
 
     case REGISTER_SUCCESS:
+      return {
+        ...state,
+        // ...action.payload,
+        loading: false,
+      }
     case LOGIN_SUCCESS:
-      localStorage.setItem('token', action.payload.token);
+      localStorage.setItem("token", action.payload.token)
       return {
         ...state,
         ...action.payload,
+        user: action.payload.data,
         isAuthenticated: true,
         loading: false,
-      };
+      }
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
     case LOGOUT:
-      localStorage.removeItem('token');
+      localStorage.removeItem("token")
+      console.log("logout dispatch")
       return {
         ...state,
         token: null,
         isAuthenticated: false,
         loading: false,
-      };
+        user: null,
+        data: null,
+        msg: "",
+      }
     default:
-      return state;
+      return state
   }
 }
