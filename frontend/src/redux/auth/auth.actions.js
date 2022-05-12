d0b371c611f16ae706fccaaf70e160081b7167cb
Krishna sai Patel <herorocks56@gmail.com>
2022-05-11 21:38:04 -0700

    auth redux changes

--- frontend/src/redux/auth/auth.actions.js
+++ frontend/src/redux/auth/auth.actions.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios"
 
-import config from "../../config";
-import setAuthToken from './auth.utils';
-import {setAlert} from '../alert/alert.actions';
+import config from "../../config"
+import setAuthToken from "./auth.utils"
+import { setAlert } from "../alert/alert.actions"
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -11,93 +11,105 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
-} from './auth.types';
+} from "./auth.types"
 
 // Load User
 export const loadUser = () => async (dispatch) => {
   if (localStorage.token) {
-    setAuthToken(localStorage.token);
+    setAuthToken(localStorage.token)
   }
   try {
-    const res = await axios.get(config.BASE_URL + `/api/auth`);
+    const res = await axios.get(config.BASE_URL + `/api/auth`)
 
     dispatch({
       type: USER_LOADED,
       payload: res.data.data,
-    });
+    })
   } catch (err) {
     dispatch({
       type: AUTH_ERROR,
-    });
+    })
   }
-};
+}
 
 // Register User
-export const register = ({username, password}) => async (dispatch) => {
-  const config_headers = {
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: "application/json",
-    },
-  };
+export const register =
+  ({ username, password }) =>
+  async (dispatch) => {
+    const config_headers = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }
 
-  const body = JSON.stringify({username, password});
+    const body = JSON.stringify({ username, password })
 
-  try {
-    const res = await axios.post(config.BASE_URL + `/api/users`, body, config_headers);
+    try {
+      const res = await axios.post(
+        config.BASE_URL + `/api/users`,
+        body,
+        config_headers
+      )
 
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data.data,
-    });
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data.data,
+      })
 
-    dispatch(setAlert(res.data.message, 'success'));
+      dispatch(setAlert(res.data.message, "success"))
 
-    dispatch(loadUser());
-  } catch (err) {
-    dispatch(setAlert(err.response.data.message, 'danger'));
+      dispatch(loadUser())
+    } catch (err) {
+      dispatch(setAlert(err.response.data.message, "danger"))
 
-    dispatch({
-      type: REGISTER_FAIL,
-    });
+      dispatch({
+        type: REGISTER_FAIL,
+      })
+    }
   }
-};
 
 // Login User
-export const login = ({username, password}) => async (dispatch) => {
-  const config_headers = {
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: "application/json",
-    },
-  };
+export const login =
+  ({ username, password }) =>
+  async (dispatch) => {
+    const config_headers = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }
 
-  const body = JSON.stringify({username, password});
+    const body = JSON.stringify({ username, password })
 
-  try {
-    const res = await axios.post(config.BASE_URL + `/api/auth`, body, config_headers);
+    try {
+      const res = await axios.post(
+        config.BASE_URL + `/api/auth`,
+        body,
+        config_headers
+      )
 
-    dispatch({
-      type: LOGIN_SUCCESS,
-      payload: res.data.data,
-    });
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data.data,
+      })
 
-    dispatch(setAlert(res.data.message, 'success'));
+      dispatch(setAlert(res.data.message, "success"))
 
-    dispatch(loadUser());
-  } catch (err) {
-    dispatch(setAlert(err.response.data.message, 'danger'));
+      dispatch(loadUser())
+    } catch (err) {
+      dispatch(setAlert(err.response.data.message, "danger"))
 
-    dispatch({
-      type: LOGIN_FAIL,
-    });
+      dispatch({
+        type: LOGIN_FAIL,
+      })
+    }
   }
-};
 
 //LOGOUT
 export const logout = () => (dispatch) => {
-  dispatch(setAlert('User has logged out', 'success'));
-  localStorage.removeItem('token');
+  dispatch(setAlert("User has logged out", "success"))
+  localStorage.removeItem("token")
 
-  dispatch({type: LOGOUT});
-};
+  dispatch({ type: LOGOUT })
+}
