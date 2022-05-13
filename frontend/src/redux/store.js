83c8c7de8395849d683a6d43b43435f112035793
Krishna sai Patel <herorocks56@gmail.com>
2022-05-12 17:39:08 -0700

    redux store persisted

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 import thunk from "redux-thunk"
 import rootReducer from "./root-reducer"
 
@@ -7,10 +9,27 @@ const initialState = {}
 
 const middleware = [thunk]
 
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 )
 
+export const persistor = persistStore(store)
+
 export default store
+
+// export default () => {
+//   let store = createStore(persistedReducer)
+//   let persistor = persistStore(store)
+//   return { store, persistor }
+// }
+
+// export default store
