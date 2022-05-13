40596feab1a2c7aa82adf7f4134910f7c07826f8
Krishna sai Patel <herorocks56@gmail.com>
2022-05-13 02:28:44 -0700

    question activity count fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import Header from './components/header/Header';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import RoutesTree from './RoutesTree';
+import Alert from './components/Alert/Alert.component';
 
 function App() {
 	return (
 		<Provider store={store}>
 			<div className="App">
 				<Header />
-				{/* <Alert /> */}
+				<Alert />
 				<RoutesTree />
 			</div>
 		</Provider>
