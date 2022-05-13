5b5614fd8819aac85ee83afd65c746d42f8cad3d
Abul Hasan <abulhasan.swe@gmail.com>
2022-05-13 11:58:22 -0700

    Implemented unaswered filter

--- frontend/src/pages/QuestionsPage/QuestionsPage.jsx
+++ frontend/src/pages/QuestionsPage/QuestionsPage.jsx
@@ -1,54 +1,51 @@
-import React, {Fragment, useState, useEffect} from 'react';
+import React, { Fragment, useState, useEffect } from "react";
 
- import {useLocation} from 'react-router-dom';
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-import {getPosts} from '../../redux/posts/posts.actions';
-import handleSorting from '../../services/handleSorting';
+import { useLocation } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getPosts } from "../../redux/posts/posts.actions";
+import handleSorting from "../../services/handleSorting";
 
-import LinkButton from '../../components/link-button/LinkButton';
-import PostItem from '../../components/PostItem/PostItem.component';
-import Spinner from '../../components/Spinner/Spinner';
-import ButtonGroup from '../../components/ButtonGroup/ButtonGroup.component';
-import SearchBox from '../../components/SearchBox/SearchBox.component';
-import PageTitle from '../../components/PageTitle/PageTitle.component';
+import LinkButton from "../../components/link-button/LinkButton";
+import PostItem from "../../components/PostItem/PostItem.component";
+import Spinner from "../../components/Spinner/Spinner";
+import ButtonGroup from "../../components/ButtonGroup/ButtonGroup.component";
+import SearchBox from "../../components/SearchBox/SearchBox.component";
+import PageTitle from "../../components/PageTitle/PageTitle.component";
 import Pagination from "../../components/Pagination/Pagination.component";
 
-import './QuestionsPage.styles.css';
+import "./QuestionsPage.styles.css";
 
 const itemsPerPage = 10;
 
-
-const QuestionsPage = ({ getPosts, post: { posts, loading },auth }) => {
-  
-  const [searchQuery] = useState(new URLSearchParams(useLocation().search).get('search'));
+const QuestionsPage = ({ getPosts, post: { posts, loading }, auth }) => {
+  const [searchQuery] = useState(
+    new URLSearchParams(useLocation().search).get("search")
+  );
   console.log();
   //setSearchQuery(new URLSearchParams(useLocation().search).get('search'));
-   useEffect(() => {
-     if(searchQuery)
-     {
+  useEffect(() => {
+    if (searchQuery) {
       getPosts(searchQuery);
-     }else{
+    } else {
       getPosts();
-     }
-
+    }
   }, [getPosts]);
 
+  console.log(posts);
 
-  console.log(posts)
-  
   const [page, setPage] = useState(1);
-  const [sortType, setSortType] = useState('Score');
+  const [sortType, setSortType] = useState("Score");
   //let searchQuery = "";
 
   const handlePaginationChange = (e, value) => setPage(value);
 
-  //return 
+  //return
   // loading || posts === null ? (
   //   <Spinner type='page' width='75px' height='200px' />
-  // ) : 
+  // ) :
   return loading || posts === null ? (
-    <Spinner type='page' width='75px' height='200px' />
+    <Spinner type="page" width="75px" height="200px" />
   ) : (
     <Fragment>
       {searchQuery ? (
@@ -56,57 +53,72 @@ const QuestionsPage = ({ getPosts, post: { posts, loading },auth }) => {
           title={`Search Results for ${searchQuery} - Stack Overflow`}
         />
       ) : (
-        ''
+        ""
       )}
-      <div id='mainbar' className='questions-page fc-black-800'>
-        <div className='questions-grid'>
-          <h3 className='questions-headline'>
-            {searchQuery ? 'Search Results' : 'All Questions'}
+      <div id="mainbar" className="questions-page fc-black-800">
+        <div className="questions-grid">
+          <h3 className="questions-headline">
+            {searchQuery ? "Search Results" : "All Questions"}
           </h3>
-          {auth && auth.user && auth.isAuthenticated && <div className='questions-btn'>
-            <LinkButton
-              text={'Ask Question'}
-              link={'/add/question'}
-              type={'s-btn__primary'}
-            />
-          </div>}
+          {auth && auth.user && auth.isAuthenticated && (
+            <div className="questions-btn">
+              <LinkButton
+                text={"Ask Question"}
+                link={"/add/question"}
+                type={"s-btn__primary"}
+              />
+            </div>
+          )}
         </div>
         {searchQuery ? (
-          <div className='search-questions'>
-            <span style={{color: '#acb2b8', fontSize: '12px'}}>
+          <div className="search-questions">
+            <span style={{ color: "#acb2b8", fontSize: "12px" }}>
               Results for {searchQuery}
             </span>
-            <SearchBox placeholder={'Search...'} name={'search'} pt={'mt8'} />
+            <SearchBox placeholder={"Search..."} name={"search"} pt={"mt8"} />
           </div>
         ) : (
-          ''
+          ""
         )}
-        <div className='questions-tabs'>
+        <div className="questions-tabs">
           <span>
-            {new Intl.NumberFormat('en-IN').format(posts?.length)} questions
+            {new Intl.NumberFormat("en-IN").format(posts?.length)} questions
           </span>
           <ButtonGroup
-            buttons={['Interesting', 'Hot', 'Score', 'Unanswered']}
+            buttons={["Interesting", "Hot", "Score", "Unanswered"]}
             selected={sortType}
             setSelected={setSortType}
           />
         </div>
-          <div className='questions'>
-            {/* {searchQuery ? () : ()} */}
-          {posts?.filter((post) => post.title.toLowerCase())  ///.includes(searchQuery ? searchQuery : '')
-            ?.sort(handleSorting(sortType))
-            .slice((page - 1) * itemsPerPage, (page - 1) * itemsPerPage + itemsPerPage)
-            .map((post, index) => (
-              <PostItem key={index} post={post} />
-            ))}
+        <div className="questions">
+          {/* {searchQuery ? () : ()} */}
+          {posts &&
+            (sortType == "Unanswered"
+              ? (posts
+                  .filter((post) => post.answersCount==0) ///.includes(searchQuery ? searchQuery : '')
+                  .sort(handleSorting(sortType))
+                  .slice(
+                    (page - 1) * itemsPerPage,
+                    (page - 1) * itemsPerPage + itemsPerPage
+                  )
+                  .map((post, index) => <PostItem key={index} post={post} />))
+              : (posts
+                  .sort(handleSorting(sortType))
+                  .slice(
+                    (page - 1) * itemsPerPage,
+                    (page - 1) * itemsPerPage + itemsPerPage
+                  )
+                  .map((post, index) => <PostItem key={index} post={post} />)))}
         </div>
-        <div className='sof-pagination'>
-        <Pagination
-          page={page}
-          itemList={posts.filter((post) => post.title.toLowerCase().includes(searchQuery ? searchQuery : ''))}
-          itemsPerPage={itemsPerPage}
-          handlePaginationChange={handlePaginationChange}
-        />
+        <div className="sof-pagination">
+          <Pagination
+            page={page}
+            itemList={posts.filter((post) =>
+              post.title.toLowerCase().includes(searchQuery ? searchQuery : "")
+            )}
+            itemsPerPage={itemsPerPage}
+            handlePaginationChange={handlePaginationChange}
+          />
         </div>
       </div>
     </Fragment>
@@ -120,8 +132,8 @@ QuestionsPage.propTypes = {
 
 const mapStateToProps = (state) => ({
   post: state.post,
-  auth: state.auth
+  auth: state.auth,
 });
 
-export default connect(mapStateToProps, {getPosts})(QuestionsPage);
+export default connect(mapStateToProps, { getPosts })(QuestionsPage);
 // export default QuestionsPage;
