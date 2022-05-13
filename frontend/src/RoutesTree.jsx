02e15ca4be6c7c697dc587bf1158cf0c25d2489d
Abul Hasan <abulhasan.swe@gmail.com>
2022-05-12 21:08:42 -0700

    Styling changes for post component, logic for approved and pending qeustions and other styling changes

--- frontend/src/RoutesTree.jsx
+++ frontend/src/RoutesTree.jsx
@@ -1,23 +1,23 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/Login/LoginPage';
-import NotFoundPage from './pages/NotFound/NotFoundPage';
-import withPageTitle from './components/withPageTitle/withPageTitle';
-import LayoutWrapper from './components/LayoutWrapper/LayoutWrapper.component';
-import RegisterPage from './pages/Register/RegisterPage';
-import AllTagsPage from './pages/AllTagsPage/AllTagsPage';
-import QuestionsPage from './pages/QuestionsPage/QuestionsPage';
-import QuestionActivity from './pages/QuestionActivity/QuestionActivity';
-import AskQuestionPage from './pages/AskQuestionPage/AskQuestionPage';
-import AdminAnalytics from './components/admin-analytics/AdminAnalytics';
-import Post from './pages/Post/Post';
-import Home from './pages/Home/Home';
-import ChatBody from './components/chatBody/ChatBody';
-import AllUsersPage from './pages/AllUsersPage/AllUsersPage.component';
-import ProfilePage from './pages/ProfilePage/ProfilePage.component';
-import AllPendingQuestions from './pages/AllPendingQuestionsPage/AllPendingQuestions';
-import UserActivityTabTags from './components/user-activity-tab-tags/UserActivityTabTags';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Login/LoginPage";
+import NotFoundPage from "./pages/NotFound/NotFoundPage";
+import withPageTitle from "./components/withPageTitle/withPageTitle";
+import LayoutWrapper from "./components/LayoutWrapper/LayoutWrapper.component";
+import RegisterPage from "./pages/Register/RegisterPage";
+import AllTagsPage from "./pages/AllTagsPage/AllTagsPage";
+import QuestionsPage from "./pages/QuestionsPage/QuestionsPage";
+import QuestionActivity from "./pages/QuestionActivity/QuestionActivity";
+import AskQuestionPage from "./pages/AskQuestionPage/AskQuestionPage";
+import AdminAnalytics from "./components/admin-analytics/AdminAnalytics";
+import Post from "./pages/Post/Post";
+import Home from "./pages/Home/Home";
+import ChatBody from "./components/chatBody/ChatBody";
+import AllUsersPage from "./pages/AllUsersPage/AllUsersPage.component";
+import ProfilePage from "./pages/ProfilePage/ProfilePage.component";
+import AllPendingQuestions from "./pages/AllPendingQuestionsPage/AllPendingQuestions";
+import UserActivityTabTags from "./components/user-activity-tab-tags/UserActivityTabTags";
 
 //import AskQuestionPage from './pages/AskQuestionPage/AskQuestionPage';
 
@@ -29,75 +29,77 @@ import UserActivityTabTags from './components/user-activity-tab-tags/UserActivit
 //import TagPage from './pages/TagPage/TagPage';
 
 const AllTagsPageComponent = withPageTitle({
-	component: LayoutWrapper({ component: AllTagsPage }),
-	title: 'Tags - CLONE Stack Overflow',
+  component: LayoutWrapper({ component: AllTagsPage }),
+  title: "Tags - CLONE Stack Overflow",
 });
 
 const QuestionsPageComponent = withPageTitle({
-	component: LayoutWrapper({ component: QuestionsPage }),
-	title: 'All Questions - Stack Overflow',
+  component: LayoutWrapper({ component: QuestionsPage }),
+  title: "All Questions - Stack Overflow",
 });
 
 const ProfilePageComponent = LayoutWrapper({ component: ProfilePage });
 
 const QuestionActivityComponent = withPageTitle({
-	component: LayoutWrapper({ component: QuestionActivity }),
-	title: 'Question Activity - Stack Overflow',
+  component: LayoutWrapper({ component: QuestionActivity }),
+  title: "Question Activity - Stack Overflow",
 });
 
 const IndividualPost = withPageTitle({
-	component: LayoutWrapper({ component: Post }),
-	title: 'Post - Stack Overflow',
+  component: LayoutWrapper({ component: Post }),
+  title: "Post - Stack Overflow",
 });
 
 const AllUsersPageComponent = withPageTitle({
-	component: LayoutWrapper({ component: AllUsersPage }),
-	title: 'Users - CLONE Stack Overflow',
+  component: LayoutWrapper({ component: AllUsersPage }),
+  title: "Users - CLONE Stack Overflow",
 });
 
 const AdminAnalyticsComponent = withPageTitle({
-	component: LayoutWrapper({ component: AdminAnalytics }),
-	title: 'Admin Analytics',
+  component: LayoutWrapper({ component: AdminAnalytics }),
+  title: "Admin Analytics",
 });
 
 const AllPendingQuestionsComponent = withPageTitle({
-	component: LayoutWrapper({ component: AllPendingQuestions }),
-	title: 'Admin Analytics',
+  component: LayoutWrapper({ component: AllPendingQuestions }),
+  title: "Admin Analytics",
 });
 
 const RoutesTree = () => {
-	return (
-		<Routes>
-			<Route exact path="/" element={<Home />} />
-			<Route
-				exact
-				path="/questions"
-				element={<QuestionsPageComponent />}
-			/>
-			<Route exact path="/add/question" element={<AskQuestionPage />} />
-			{/* <Route exact path='/tags/:tagname' component={TagPageComponent} /> */}
-			<Route exact path="/questions/:id" element={<IndividualPost />} />
-			<Route
-				exact
-				path="/questions/timeline/:id"
-				element={<QuestionActivityComponent />}
-			/>
-			<Route exact path="/users/:id" element={<ProfilePageComponent />} />
-			<Route exact path="/allusers" element={<AllUsersPageComponent />} />
-			<Route exact path="/tags" element={<AllTagsPageComponent />} />
-			<Route exact path="/register" element={<RegisterPage />} />
-			<Route exact path="/login" element={<LoginPage />} />
-			<Route
-				exact
-				path="/admin/analytics"
-				element={<AdminAnalyticsComponent />}
-			/>
-			<Route exact path="/admin/pending-questions" element ={<AllPendingQuestionsComponent/>}/>
-			<Route path="*" element={<NotFoundPage />} />
-			<Route path="/messages" element={<ChatBody />} />
-			<Route path="/tags-activity" element={<UserActivityTabTags />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/questions" element={<QuestionsPageComponent />} />
+      <Route exact path="/add/question" element={<AskQuestionPage />} />
+      {/* <Route exact path='/tags/:tagname' component={TagPageComponent} /> */}
+      <Route exact path="/questions/:id" element={<IndividualPost />} />
+      <Route
+        exact
+        path="/questions/timeline/:id"
+        element={<QuestionActivityComponent />}
+      />
+      <Route exact path="/users/:id" element={<ProfilePageComponent />} />
+      <Route exact path="/allusers" element={<AllUsersPageComponent />} />
+      <Route exact path="/tags" element={<AllTagsPageComponent />} />
+      <Route exact path="/register" element={<RegisterPage />} />
+      <Route exact path="/login" element={<LoginPage />} />
+      <Route
+        exact
+        path="/admin/analytics"
+        element={<AdminAnalyticsComponent />}
+      />
+      <Route
+        exact
+        path="/admin/pending-questions"
+        element={<AllPendingQuestionsComponent />}
+      />
+      <Route exact path="/admin/pending-questions/:id" element={<IndividualPost />} />
+
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/messages" element={<ChatBody />} />
+      <Route path="/tags-activity" element={<UserActivityTabTags />} />
+    </Routes>
+  );
 };
 
 export default RoutesTree;
--- frontend/src/components/PostItem/PostItem.component.jsx
+++ frontend/src/components/PostItem/PostItem.component.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
-import htmlSubstring from '../../services/htmlSubstring'
-import injectEllipsis from '../../services/injectEllipsis'
+// import htmlSubstring from '../../services/htmlSubstring'
+// import injectEllipsis from '../../services/injectEllipsis'
 
 import UserCard from '../UserCard/UserCard.component';
 import TagBadge from '../TagBadge/TagBadge.component';
@@ -15,7 +15,7 @@ const PostItem = ({
   post: {
     _id,
     title,
-    descr,
+    // descr,
     createdBy,
     answer_count,
     // comment_count,
@@ -45,7 +45,7 @@ const PostItem = ({
         <div className='stats'>
           <div className='vote'>
             <span className='vote-count'>{views}</span>
-            <div className='count-text'>views</div>
+            <span className='count-text'>views</span>
           </div>
           {answer_count > 0 ? answerVoteUp : answerVoteDown}
           <div className='vote'>
@@ -61,7 +61,7 @@ const PostItem = ({
         <h3>
           {isAdmin?<Link to={`/admin/pending-questions/${_id}`}>{title}</Link>:<Link to={`/questions/${_id}`}>{title}</Link>}
         </h3>
-        <div className='brief' dangerouslySetInnerHTML={{__html: injectEllipsis(htmlSubstring(descr, 200))}}></div>
+        {/* <div className='brief' dangerouslySetInnerHTML={{__html: injectEllipsis(htmlSubstring(descr, 200))}}></div> */}
         {tags.map((tag, index) => (
           <TagBadge key={index} tag_name={tag.name} size={'s-tag'} float={'left'} />
         ))}
