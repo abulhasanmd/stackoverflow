c2d3e74f484cbf3f6fa8c3462ee9a085e9ff5d18
Krishna sai Patel <herorocks56@gmail.com>
2022-05-12 17:39:30 -0700

    comments redux modified

--- frontend/src/redux/comments/comments.actions.js
+++ frontend/src/redux/comments/comments.actions.js
@@ -1,89 +1,96 @@
-import axios from 'axios';
+import axios from "axios"
 
-import config from "../../config";
-import {setAlert} from '../alert/alert.actions';
+import config from "../../config"
+import { setAlert } from "../alert/alert.actions"
 import {
   GET_COMMENTS,
   COMMENT_ERROR,
   ADD_COMMENT,
   DELETE_COMMENT,
-  GET_ANSWER_COMMENTS
-} from './comments.types';
+  GET_ANSWER_COMMENTS,
+  LOADING_COMMENT,
+} from "./comments.types"
 
-export const getComments = (id, type="question") => async (dispatch) => {
-  try {
-    const res = await axios.get(config.BASE_URL + `/comment/get-comment-by-resourceid/${id}`);
-    console.log("comments data",res);
-    if(type === "answer"){
+export const getComments =
+  (id, type = "question") =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: LOADING_COMMENT })
+      const res = await axios.get(
+        config.BASE_URL + `/comment/get-comment-by-resourceid/${id}`
+      )
+      console.log("comments data", res)
+      if (type === "answer") {
+        dispatch({
+          type: GET_ANSWER_COMMENTS,
+          payload: res.data.data,
+        })
+      } else {
+        dispatch({
+          type: GET_COMMENTS,
+          payload: res.data.data,
+        })
+      }
+    } catch (err) {
       dispatch({
-        type: GET_ANSWER_COMMENTS,
-        payload: res.data.data,
-      });
-    }else{
-      dispatch({
-        type: GET_COMMENTS,
-        payload: res.data.data,
-      });
+        type: COMMENT_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      })
     }
-    
-  } catch (err) {
-    dispatch({
-      type: COMMENT_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status},
-    });
   }
-};
 
 // Add COMMENT
 export const addComment = (formData) => async (dispatch) => {
   const config_headers = {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  };
+  }
 
   try {
     const res = await axios.post(
       config.BASE_URL + `/comment/post-comment`,
       formData,
       config_headers
-    );
+    )
 
     dispatch({
       type: ADD_COMMENT,
       payload: res.data.data,
-    });
+    })
 
-    dispatch(setAlert(res.data.message, 'success'));
+    dispatch(setAlert(res.data.message, "success"))
 
-    dispatch(getComments(formData.resourceId));
+    dispatch(getComments(formData.resourceId))
   } catch (err) {
     // dispatch(setAlert(err.response.data.message, 'danger'));
-    console.log("error is",err);
+    console.log("error is", err)
     dispatch({
       type: COMMENT_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status},
-    });
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
   }
-};
+}
 
 // Delete Comment
 export const deleteComment = (CommentId) => async (dispatch) => {
   try {
-    const res = await axios.delete(config.BASE_URL + `/api/posts/comments/${CommentId}`);
+    const res = await axios.delete(
+      config.BASE_URL + `/api/posts/comments/${CommentId}`
+    )
 
     dispatch({
       type: DELETE_COMMENT,
       payload: CommentId,
-    });
+    })
 
-    dispatch(setAlert(res.data.message, 'success'));
+    dispatch(setAlert(res.data.message, "success"))
   } catch (err) {
-    dispatch(setAlert(err.response.data.message, 'danger'));
+    dispatch(setAlert(err.response.data.message, "danger"))
 
     dispatch({
       type: COMMENT_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status},
-    });
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
   }
-};
+}
