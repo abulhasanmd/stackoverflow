90c70667583c6231d2fcb6a4a5ba7352e0d4c5ea
Abul Hasan <abulhasan.swe@gmail.com>
2022-05-13 14:22:08 -0700

    corrected redux error

--- frontend/src/redux/posts/posts.reducer.js
+++ frontend/src/redux/posts/posts.reducer.js
@@ -11,14 +11,14 @@ import {
   ADD_VOTE_TO_POST,
   ADD_VOTE_TO_POST_ERROR,
   LOADING_POST,
-} from "./posts.types"
+} from "./posts.types";
 
 const initialState = {
   posts: [],
   post: null,
   loading: true,
   error: {},
-}
+};
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
@@ -26,12 +26,13 @@ export default function posts(state = initialState, action) {
       return {
         ...state,
         loading: true,
-      }
+      };
     case ADD_POST_TO_BOOKMARK:
-        break;
+      return state;
     case GET_POSTS:
     case ADD_POST_TO_BOOKMARK_ERROR:
     case ADD_VOTE_TO_POST:
+      return state;
     case ADD_VOTE_TO_POST_ERROR:
     case GET_TOP_POSTS:
     case GET_TAG_POSTS:
@@ -39,32 +40,32 @@ export default function posts(state = initialState, action) {
         ...state,
         posts: action.payload.posts,
         loading: false,
-      }
+      };
     case GET_POST:
       return {
         ...state,
         post: action.payload,
         loading: false,
-      }
+      };
     case ADD_POST:
       return {
         ...state,
         posts: [action.payload, ...state.posts],
         loading: false,
-      }
+      };
     case DELETE_POST:
       return {
         ...state,
         posts: state.posts.filter((post) => post.id !== action.payload),
         loading: false,
-      }
+      };
     case POST_ERROR:
       return {
         ...state,
         error: action.payload,
         loading: false,
-      }
+      };
     default:
-      return state
+      return state;
   }
 }
